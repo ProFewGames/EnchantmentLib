293393a2cf454343d3395b11a4ee3a299312260d
ProFewGames <rickbrook247@gmail.com>
2022-03-24 19:36:24 -0400

    Cleaner logging messages

--- EnchantmentLib-API/src/main/java/xyz/ufactions/enchantmentlib/EnchantmentLib.java
+++ EnchantmentLib-API/src/main/java/xyz/ufactions/enchantmentlib/EnchantmentLib.java
@@ -10,15 +10,15 @@ import java.util.Locale;
 
 public class EnchantmentLib {
 
+    private static JavaPlugin loaderPlugin;
     private static Enchantment glowEnchantment;
 
     public static Enchantment getGlowEnchantment() {
         if (glowEnchantment == null) {
-            Plugin plugin = JavaPlugin.getProvidingPlugin(EnchantmentLib.class);
             if (VersionUtils.getVersion().equalOrGreater(VersionUtils.Version.V1_13)) { // Check if enchantment is already registered.
                 glowEnchantment = Enchantment.getByKey(new NamespacedKey("CustomCrates".toLowerCase(Locale.ROOT), "CustomCratesGlow".toLowerCase(Locale.ROOT)));
-                if (glowEnchantment != null){
-                    debug(plugin, "Glow enchantment already initialized. Using located enchantment.");
+                if (glowEnchantment != null) {
+                    debug("Glow enchantment already initialized. Using located enchantment.");
                     return glowEnchantment;
                 }
             }
@@ -26,18 +26,18 @@ public class EnchantmentLib {
                 Field EnchantmentFieldAcceptingNew = Enchantment.class.getDeclaredField("acceptingNew");
                 EnchantmentFieldAcceptingNew.setAccessible(true);
                 EnchantmentFieldAcceptingNew.set(null, true);
-                debug(plugin, "Version Detected: " + VersionUtils.getVersion().name());
+                debug("Version Detected: " + VersionUtils.getVersion().name());
                 if (VersionUtils.getVersion().equalOrGreater(VersionUtils.Version.V1_13)) {
-                    debug(plugin, "Using latest enchantment api");
+                    debug("Using latest enchantment api");
                     glowEnchantment = new LatestGlowEnchantment();
                 } else {
-                    debug(plugin, "Using legacy enchantment api");
+                    debug("Using legacy enchantment api");
                     glowEnchantment = new LegacyGlowEnchantment();
                 }
                 Enchantment.registerEnchantment(glowEnchantment);
             } catch (IllegalAccessException | NoSuchFieldException e) {
-                glowEnchantment = Enchantment.values()[0];
-                debug(plugin, "Cannot register glow enchantment. " +
+                glowEnchantment = null;
+                debug("Cannot register glow enchantment. " +
                         "Has the plugin been reloaded? " +
                         "Please provide the following error when creating a support ticket:");
                 e.printStackTrace();
@@ -46,7 +46,14 @@ public class EnchantmentLib {
         return glowEnchantment;
     }
 
-    private static void debug(Plugin plugin, String message) {
-        plugin.getLogger().info("[EnchantmentLib] " + message);
+    private static Plugin getLoaderPlugin() {
+        if (loaderPlugin == null) {
+            loaderPlugin = JavaPlugin.getProvidingPlugin(EnchantmentLib.class);
+        }
+        return loaderPlugin;
+    }
+
+    private static void debug(String message) {
+        getLoaderPlugin().getLogger().info("[EnchantmentLib] " + message);
     }
 }
